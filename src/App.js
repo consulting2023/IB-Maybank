bbafd2b5d5a631ee2f40bfc207328b243c181988
Raphael S. Rodrigues <rapa.rodrigues@gmail.com>
2024-10-31 14:29:41 -0300

    [feature] [style] performance de trocar a senha

--- src/App.js
+++ src/App.js
@@ -1,61 +1,64 @@
-import React from 'react';
+import React from "react";
 import "react-datepicker/dist/react-datepicker.css";
-import 'react-pro-sidebar/dist/css/styles.css';
-import { BrowserRouter } from 'react-router-dom';
-import Router from './routers/router';
-import Header from './components/header/Header';
-import Sidebar from './components/sidemenu/Sidebar';
-import AlertOverlay from './components/alertOverlay/AlertOverlay';
-import './templates/App.scss';
+import "react-pro-sidebar/dist/css/styles.css";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routers/router";
+import Header from "./components/header/Header";
+import Sidebar from "./components/sidemenu/Sidebar";
+import AlertOverlay from "./components/alertOverlay/AlertOverlay";
+import "./templates/App.scss";
 
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row } from "react-bootstrap";
 
 function App() {
-  document.title = process.env.NOME_BANCO
+  document.title = process.env.NOME_BANCO;
 
   var privatePaths = [
-    '/home',
-    '/pix',
-    '/extrato',
-    '/extrato_conta',
-    '/extrato_cartao',
-    '/extrato_convenio',
-    '/extrato_investimento',
-    '/extrato_parcelado',
-    '/extrato_financeira',
-    '/transferencia',
-    '/transferencia_interna',
-    '/transferencia_outros',
-    '/transferencia_lote',
-    '/cobrancas',
-    '/cobrancas_boleto',
-    '/multiplos_boletos',
-    '/GerenciarLotesBoletos',
-    '/Gerenciar_Boletos',
-    '/Cadastro_Sacado',
-    '/Editar_Sacado',
-    '/Pagar',
-    '/deposito',
-    '/gerenciar_deposito',
-    '/comprovantes',
-    '/comprovantes_conta',
-    '/cartoes',
-    '/servicos',
-    '/recarga',
-    '/entretenimento',
-    '/games',
-    '/aplicativos',
-    '/comprovantes_cartao',
-    '/comprovantes_investimento',
-    '/comprovantes_convenio',
-    '/comprovantes_limite',
-    '/comprovantes_parcelado',
-    '/comprovantes_servico',
-    '/qr_code',
-    '/perfil'
-  ]
+    "/home",
+    "/pix",
+    "/extrato",
+    "/extrato_conta",
+    "/extrato_cartao",
+    "/extrato_convenio",
+    "/extrato_investimento",
+    "/extrato_parcelado",
+    "/extrato_financeira",
+    "/transferencia",
+    "/transferencia_interna",
+    "/transferencia_outros",
+    "/transferencia_lote",
+    "/cobrancas",
+    "/cobrancas_boleto",
+    "/multiplos_boletos",
+    "/GerenciarLotesBoletos",
+    "/Gerenciar_Boletos",
+    "/Cadastro_Sacado",
+    "/Editar_Sacado",
+    "/Pagar",
+    "/deposito",
+    "/gerenciar_deposito",
+    "/comprovantes",
+    "/comprovantes_conta",
+    "/cartoes",
+    "/servicos",
+    "/recarga",
+    "/entretenimento",
+    "/games",
+    "/aplicativos",
+    "/comprovantes_cartao",
+    "/comprovantes_investimento",
+    "/comprovantes_convenio",
+    "/comprovantes_limite",
+    "/comprovantes_parcelado",
+    "/comprovantes_servico",
+    "/qr_code",
+    "/perfil",
+    "/senha",
+  ];
 
-  var isPrivate = (privatePaths.includes(document.location.pathname)) ? true : false;
+  var isPrivate = privatePaths.includes(document.location.pathname)
+    ? true
+    : false;
 
   const [lang, setLang] = React.useState(null);
   const traduzir = async (data) => {
@@ -66,7 +69,7 @@ function App() {
   const [count, setCount] = React.useState(0);
 
   const createAlert = async (alertTitle, alertText, alertType) => {
-    if (alerts.length>=30) setAlerts(alerts.shift());
+    if (alerts.length >= 30) setAlerts(alerts.shift());
     const newKey = alerts.length;
     const newAlert = {
       key: newKey,
@@ -74,13 +77,13 @@ function App() {
       title: alertTitle,
       text: alertText,
       type: alertType,
-    }
+    };
     setAlerts([...alerts, newAlert]);
     setTimeout(() => {
       setCount([newKey]);
     }, 30000);
     //30 segundos
-  }
+  };
 
   React.useEffect(() => {
     closeAlert(count);
@@ -88,28 +91,22 @@ function App() {
 
   const closeAlert = async (index) => {
     let alertList = [...alerts];
-    if (alertList[index]){
+    if (alertList[index]) {
       alertList[index].show = false;
       setAlerts(alertList);
     }
-  }
+  };
 
   return (
     <BrowserRouter>
       {/* <div className="site-content container-fluid m-0 p-0 min-vh-100 h-100 position-relative"> */}
       <div className="site-content container-fluid m-0 p-0 min-vh-100 min-w-100 position-relative">
-        <AlertOverlay alerts={alerts} closeProp={closeAlert}/>
-        { 
-          isPrivate && 
-          <Header/> 
-        }
+        <AlertOverlay alerts={alerts} closeProp={closeAlert} />
+        {isPrivate && <Header />}
         <Row className="m-0 p-0 min-vh-100">
-          {
-            isPrivate && 
-            <Sidebar langProp={traduzir}/>
-          }
-          <Col className={"m-0 px-0 " + (isPrivate && "pb-5") }>
-            <Router className="" langProp={traduzir} alerts={createAlert}/>
+          {isPrivate && <Sidebar langProp={traduzir} />}
+          <Col className={"m-0 px-0 " + (isPrivate && "pb-5")}>
+            <Router className="" langProp={traduzir} alerts={createAlert} />
           </Col>
         </Row>
       </div>
--- src/pages/senha/senha.js
+++ src/pages/senha/senha.js
@@ -26,12 +26,14 @@ export default class Senha extends Component {
     this.setState({ nivel });
   }
 
-  modalSenha = (id) => {
-    if (id === 1) {
-      this.setState({ modalShowLogin: true });
-    } else if (id === 2) {
-      this.setState({ modalShowTransfer: true });
-    }
+  modalSenhaLogin = () => {
+    this.setState({ modalShowLogin: true });
+
+    this.enviarToken();
+  };
+  modalSenhaTransfer = () => {
+    this.setState({ modalShowLogin: true });
+
     this.enviarToken();
   };
 
@@ -54,49 +56,67 @@ export default class Senha extends Component {
   };
 
   trocarSenha = () => {
-    const data = {
-      url: "usuario/troca-senha",
-      data: {
-        email: Funcoes.pessoa.email,
-        token: this.state.OTP,
-        senha: this.state.senha,
-      },
-      method: "POST",
-      console: false,
-      tipo_aparelho: global.tipoaparelho,
-      funcao: "salvar_nova",
-      tela: "recuperar",
-    };
+    if (this.state.senha == "") {
+      alert("É Obrigatorio preencher a senha para seguir adiante");
+    } else if (this.state.OTP == "") {
+      alert("É Obrigatorio preencher a o token para seguir adiante");
+    } else {
+      const data = {
+        url: "usuario/troca-senha",
+        data: {
+          email: Funcoes.pessoa.email,
+          token: this.state.OTP,
+          senha: this.state.senha,
+        },
+        method: "POST",
+        console: false,
+        tipo_aparelho: global.tipoaparelho,
+        funcao: "salvar_nova",
+        tela: "recuperar",
+      };
 
-    Funcoes.Geral_API(data, true).then((res) => {
-      if (res === 1) {
-        alert("Senha alterada");
-        location.reload();
-      }
-    });
+      Funcoes.Geral_API(data, true).then((res) => {
+        if (res === 1) {
+          alert("Senha alterada com sucesso! Sera preciso Logar novamente");
+          Funcoes.logout();
+        } else {
+          alert("Erro em alterar a senha, tente novamente mais tarde");
+          location.reload;
+        }
+      });
+    }
   };
 
   trocarSenhaTransfer = () => {
-    const data = {
-      url: "usuario/troca-senha-transferencia",
-      data: {
-        email: Funcoes.pessoa.email,
-        token: this.state.OTP,
-        senha: this.state.senhaTransfer,
-      },
-      method: "POST",
-      console: false,
-      tipo_aparelho: global.tipoaparelho,
-      funcao: "salvar_nova",
-      tela: "recuperar",
-    };
+    if (this.state.senha == "") {
+      alert("É Obrigatorio preencher a senha para seguir adiante");
+    } else if (this.state.OTP == "") {
+      alert("É Obrigatorio preencher a o token para seguir adiante");
+    } else {
+      const data = {
+        url: "usuario/troca-senha-transferencia",
+        data: {
+          email: Funcoes.pessoa.email,
+          token: this.state.OTP,
+          senha: this.state.senhaTransfer,
+        },
+        method: "POST",
+        console: false,
+        tipo_aparelho: global.tipoaparelho,
+        funcao: "salvar_nova",
+        tela: "recuperar",
+      };
 
-    Funcoes.Geral_API(data, true).then((res) => {
-      if (res === 1) {
-        alert("Senha alterada com sucesso!");
-        location.reload();
-      }
-    });
+      Funcoes.Geral_API(data, true).then((res) => {
+        if (res === 1) {
+          alert("Senha alterada com sucesso!");
+          location.reload;
+        } else {
+          alert("Erro em alterar a senha, tente novamente mais tarde");
+          location.reload;
+        }
+      });
+    }
   };
 
   render() {
@@ -122,7 +142,7 @@ export default class Senha extends Component {
                 <Button
                   variant="outline-primary"
                   className="baseButtonPrimary"
-                  onClick={() => this.modalSenha(1)}
+                  onClick={() => this.modalSenhaLogin()}
                 >
                   <Row className="w-80 m-auto">
                     <Col xs={5} className="m-auto px-0">
@@ -134,22 +154,26 @@ export default class Senha extends Component {
                   </Row>
                 </Button>
               </Col>
-              <Col className="p-3">
-                <Button
-                  variant="outline-primary"
-                  className="baseButtonPrimary"
-                  onClick={() => this.modalSenha(2)}
-                >
-                  <Row className="w-80 m-auto">
-                    <Col xs={5} className="m-auto px-0">
-                      {Icones.senhaTransfer}
-                    </Col>
-                    <Col xs={7} className="px-0 my-auto">
-                      <p className="buttonTitle m-auto">Senha de Transferência</p>
-                    </Col>
-                  </Row>
-                </Button>
-              </Col>
+              {this.state.nivel !== "400" && (
+                <Col className="p-3">
+                  <Button
+                    variant="outline-primary"
+                    className="baseButtonPrimary"
+                    onClick={this.modalSenhaTransfer}
+                  >
+                    <Row className="w-80 m-auto">
+                      <Col xs={5} className="m-auto px-0">
+                        {Icones.senhaTransfer}
+                      </Col>
+                      <Col xs={7} className="px-0 my-auto">
+                        <p className="buttonTitle m-auto">
+                          Senha de Transferência
+                        </p>
+                      </Col>
+                    </Row>
+                  </Button>
+                </Col>
+              )}
             </Row>
           </Col>
         </Container>
@@ -182,9 +206,14 @@ export default class Senha extends Component {
                 <h6>Senha Nova</h6>
               </Row>
               <Row>
-                <input
-                  maxLength={6}
-                  onChange={(e) => this.setState({ senha: e.target.value })}
+                <OtpInput
+                  focusInput={1}
+                  isInputNum={true}
+                  value={this.state.senha}
+                  onChange={(value) => this.setState({ senha: value })}
+                  numInputs={6}
+                  className="tokenValidacao"
+                  inputType="password"
                 />
               </Row>
             </Container>
@@ -197,51 +226,51 @@ export default class Senha extends Component {
         </Modal>
 
         {/* Modal Senha Transferência */}
-        {this.state.nivel !== "400" && (
-          <Modal
-            show={this.state.modalShowTransfer}
-            size="lg"
-            onHide={() => this.setState({ modalShowTransfer: false })}
-          >
-            <Modal.Header closeButton>
-              <Modal.Title>Senha Transação</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Container>
-                <Row>
-                  <h3>Token enviado para seu email</h3>
-                </Row>
-                <Row>
-                  <OtpInput
-                    focusInput={1}
-                    isInputNum={true}
-                    value={this.state.OTP}
-                    onChange={(value) => this.setState({ OTP: value })}
-                    numInputs={6}
-                    className="tokenValidacao"
-                  />
-                </Row>
-                <Row>
-                  <h6>Senha Transação Nova</h6>
-                </Row>
-                <Row>
-                  <input
-                    maxLength={6}
-                    onChange={(e) => this.setState({ senhaTransfer: e.target.value })}
-                  />
-                </Row>
-              </Container>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button
-                variant="primary"
-                onClick={this.trocarSenhaTransfer}
-              >
-                Confirmar
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        )}
+
+        <Modal
+          show={this.state.modalShowTransfer}
+          size="lg"
+          onHide={() => this.setState({ modalShowTransfer: false })}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Senha Transação</Modal.Title>
+          </Modal.Header>
+          <Modal.Body style="align-items: center">
+            <Container>
+              <Row>
+                <h3>Token enviado para seu email</h3>
+              </Row>
+              <Row>
+                <OtpInput
+                  focusInput={1}
+                  isInputNum={true}
+                  value={this.state.OTP}
+                  onChange={(value) => this.setState({ OTP: value })}
+                  numInputs={6}
+                  className="tokenValidacao"
+                />
+              </Row>
+              <Row>
+                <h6>Senha Transação Nova</h6>
+              </Row>
+              <Row>
+                <OtpInput
+                  focusInput={1}
+                  isInputNum={true}
+                  value={this.state.senhaTransfer}
+                  onChange={(value) => this.setState({ senhaTransfer: value })}
+                  numInputs={6}
+                  className="tokenValidacao"
+                />
+              </Row>
+            </Container>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={this.trocarSenhaTransfer}>
+              Confirmar
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
