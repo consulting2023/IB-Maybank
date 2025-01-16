07f13c3223908015f51346a0aedfc171f164c059
Raphael Souza Rodrigues <rapa.rodrigues@gmail.com>
2025-01-16 16:45:51 -0300

    [feature] tradutor

--- src/tradutor/tradutor.js
+++ src/tradutor/tradutor.js
@@ -1,24 +1,24 @@
-import i18n from 'i18n-js';
-import ptBr from './pt-BR';
-import chCh from './ch-CH';
+import i18n from "i18n-js";
+import ptBr from "./pt-BR";
+import chCh from "./ch-CH";
+import enUS from "./english";
+import esPN from "./spanish";
 
-const langArr = [
-    ptBr,
-    chCh,
-];
+const langArr = [ptBr, chCh, enUS, esPN];
 
 for (var i = 0, len = langArr.length; i < len; i++) {
-    i18n.translations[langArr[i].langInfo.sigla] = langArr[i];
-};
+  i18n.translations[langArr[i].langInfo.sigla] = langArr[i];
+}
 
 const setLanguageToI18n = async () => {
-    i18n.defaultLocale = langArr[0].langInfo.sigla;
+  i18n.defaultLocale = langArr[0].langInfo.sigla;
 
-    let langCode = localStorage.getItem('lang');
-    (langArr.filter(e => e.langInfo.sigla === langCode).length < 1) && (langCode = langArr[0].langInfo.sigla);
-    localStorage.setItem('lang', langCode);
-    i18n.locale = langCode;
-}
+  let langCode = localStorage.getItem("lang");
+  langArr.filter((e) => e.langInfo.sigla === langCode).length < 1 &&
+    (langCode = langArr[0].langInfo.sigla);
+  localStorage.setItem("lang", langCode);
+  i18n.locale = langCode;
+};
 
 setLanguageToI18n();
 
